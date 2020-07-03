65f4b8638af5e625d29f8f53b5137f00cabd4dda
Redhwan Nacef <nacefredhwan@gmail.com>
2020-07-03 15:24:04 +0100

    Add Moving Elements to excalidraw tutorial

--- tils/excalidraw-tutorial/README.md
+++ tils/excalidraw-tutorial/README.md
@@ -4,6 +4,8 @@ A tutorial on how to build a drawing app, inspired by [Excalidraw](https://excal
 
 Useful links:
 
+**Part 1:**
+
 Youtube Tutorial: https://youtu.be/6arkndScw7A
 
 Excalidraw: https://excalidraw.com/
@@ -11,3 +13,9 @@ Excalidraw: https://excalidraw.com/
 Roughjs: https://roughjs.com/
 
 HTML Canvas (MDN): https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+
+**Part 2 (Moving Elements):**
+
+Youtube Tutorial (Moving Elements): https://youtu.be/IcfhcJrtJqI
+
+Check if a point is on a line: https://stackoverflow.com/a/17693146/5041141
--- tils/excalidraw-tutorial/src/App.js
+++ tils/excalidraw-tutorial/src/App.js
@@ -3,18 +3,42 @@ import rough from "roughjs/bundled/rough.esm";
 
 const generator = rough.generator();
 
-function createElement(x1, y1, x2, y2, type) {
+function createElement(id, x1, y1, x2, y2, type) {
   const roughElement =
     type === "line"
       ? generator.line(x1, y1, x2, y2)
       : generator.rectangle(x1, y1, x2 - x1, y2 - y1);
-  return { x1, y1, x2, y2, roughElement };
+  return { id, x1, y1, x2, y2, type, roughElement };
 }
+
+const isWithinElement = (x, y, element) => {
+  const { type, x1, x2, y1, y2 } = element;
+  if (type === "rectangle") {
+    const minX = Math.min(x1, x2);
+    const maxX = Math.max(x1, x2);
+    const minY = Math.min(y1, y2);
+    const maxY = Math.max(y1, y2);
+    return x >= minX && x <= maxX && y >= minY && y <= maxY;
+  } else {
+    const a = { x: x1, y: y1 };
+    const b = { x: x2, y: y2 };
+    const c = { x, y };
+    const offset = distance(a, b) - (distance(a, c) + distance(b, c));
+    return Math.abs(offset) < 1;
+  }
+};
+
+const distance = (a, b) => Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+
+const getElementAtPosition = (x, y, elements) => {
+  return elements.find(element => isWithinElement(x, y, element));
+};
 
 const App = () => {
   const [elements, setElements] = useState([]);
-  const [drawing, setDrawing] = useState(false);
-  const [elementType, setElementType] = useState("line");
+  const [action, setAction] = useState("none");
+  const [tool, setTool] = useState("line");
+  const [selectedElement, setSelectedElement] = useState(null);
 
   useLayoutEffect(() => {
     const canvas = document.getElementById("canvas");
@@ -26,30 +50,59 @@ const App = () => {
     elements.forEach(({ roughElement }) => roughCanvas.draw(roughElement));
   }, [elements]);
 
-  const handleMouseDown = (event) => {
-    setDrawing(true);
-
-    const { clientX, clientY } = event;
-
-    const element = createElement(clientX, clientY, clientX, clientY, elementType);
-    setElements((prevState) => [...prevState, element]);
-  };
-
-  const handleMouseMove = (event) => {
-    if (!drawing) return;
-
-    const { clientX, clientY } = event;
-    const index = elements.length - 1;
-    const { x1, y1 } = elements[index];
-    const updatedElement = createElement(x1, y1, clientX, clientY, elementType);
+  const updateElement = (id, x1, y1, x2, y2, type) => {
+    const updatedElement = createElement(id, x1, y1, x2, y2, type);
 
     const elementsCopy = [...elements];
-    elementsCopy[index] = updatedElement;
+    elementsCopy[id] = updatedElement;
     setElements(elementsCopy);
   };
 
+  const handleMouseDown = event => {
+    const { clientX, clientY } = event;
+    if (tool === "selection") {
+      const element = getElementAtPosition(clientX, clientY, elements);
+      if (element) {
+        const offsetX = clientX - element.x1;
+        const offsetY = clientY - element.y1;
+        setSelectedElement({ ...element, offsetX, offsetY });
+        setAction("moving");
+      }
+    } else {
+      const id = elements.length;
+      const element = createElement(id, clientX, clientY, clientX, clientY, tool);
+      setElements(prevState => [...prevState, element]);
+
+      setAction("drawing");
+    }
+  };
+
+  const handleMouseMove = event => {
+    const { clientX, clientY } = event;
+
+    if (tool === "selection") {
+      event.target.style.cursor = getElementAtPosition(clientX, clientY, elements)
+        ? "move"
+        : "default";
+    }
+
+    if (action === "drawing") {
+      const index = elements.length - 1;
+      const { x1, y1 } = elements[index];
+      updateElement(index, x1, y1, clientX, clientY, tool);
+    } else if (action === "moving") {
+      const { id, x1, x2, y1, y2, type, offsetX, offsetY } = selectedElement;
+      const width = x2 - x1;
+      const height = y2 - y1;
+      const newX1 = clientX - offsetX;
+      const newY1 = clientY - offsetY;
+      updateElement(id, newX1, newY1, newX1 + width, newY1 + height, type);
+    }
+  };
+
   const handleMouseUp = () => {
-    setDrawing(false);
+    setAction("none");
+    setSelectedElement(null);
   };
 
   return (
@@ -57,16 +110,18 @@ const App = () => {
       <div style={{ position: "fixed" }}>
         <input
           type="radio"
-          id="line"
-          checked={elementType === "line"}
-          onChange={() => setElementType("line")}
+          id="selection"
+          checked={tool === "selection"}
+          onChange={() => setTool("selection")}
         />
+        <label htmlFor="selection">Selection</label>
+        <input type="radio" id="line" checked={tool === "line"} onChange={() => setTool("line")} />
         <label htmlFor="line">Line</label>
         <input
           type="radio"
           id="rectangle"
-          checked={elementType === "rectangle"}
-          onChange={() => setElementType("rectangle")}
+          checked={tool === "rectangle"}
+          onChange={() => setTool("rectangle")}
         />
         <label htmlFor="rectangle">Rectangle</label>
       </div>
