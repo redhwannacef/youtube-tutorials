22c67bf3c27a5cc724e789ae1f36792b2c9f96f7
Redhwan Nacef <nacefredhwan@gmail.com>
2022-04-13 16:29:52 +0100

    Add crodwin tutorial

---
+++ crowdin-tutorial/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState, Suspense } from "react";
+import "./App.css";
+import i18n from "i18next";
+import HttpBackend from "i18next-http-backend";
+import { initReactI18next, Trans, useTranslation } from "react-i18next";
+
+i18n
+  .use(initReactI18next)
+  .use(HttpBackend)
+  .init({
+    backend: { loadPath: "/translations/{{lng}}.json" },
+    lng: "en",
+    fallbackLng: "en",
+    interpolation: { escapeValue: false },
+  });
+
+const App = () => {
+  const { t } = useTranslation();
+  const [count, setCount] = useState(0);
+
+  const onChange = (event) => {
+    i18n.changeLanguage(event.target.value);
+    setCount((previousCount) => previousCount + 1);
+  };
+
+  return (
+    <Suspense fallback="Loading...">
+      <div className="App">
+        <header className="App-header">
+          <h1>{t("welcome")}</h1>
+          <p>
+            <Trans components={{ bold: <strong />, italics: <i /> }}>
+              sample
+            </Trans>
+          </p>
+          <p>{t("changed", { count })}</p>
+          <select name="language" onChange={onChange}>
+            <option value="en">English</option>
+            <option value="fr">French</option>
+          </select>
+        </header>
+      </div>
+    </Suspense>
+  );
+};
+
+export default App;
