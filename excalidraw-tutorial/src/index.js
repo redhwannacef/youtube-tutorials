2264d30e8e970a57cbdb2421fc32dbc6d4758ea3
Redhwan Nacef <nacefredhwan@gmail.com>
2023-07-21 16:42:44 +0100

    Update excalidraw tutorial dependencies

--- excalidraw-tutorial/src/index.js
+++ excalidraw-tutorial/src/index.js
@@ -1,17 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
